0dbab6e01d65088b0f621213a15d76438f46644b
Hunter McReynolds <mcreynoldshd@gmail.com>
2022-06-27 16:58:15 -0500

    initial

--- create_schema.sql
+++ create_schema.sql
@@ -7,3 +7,21 @@ CREATE TABLE students (
   birthdate    date NOT NULL,
   address_id   integer
 );
+CREATE TABLE addresses (
+  id           serial PRIMARY KEY,
+  line_1   varchar(255) NOT NULL,
+  city    varchar(255) NOT NULL,
+  state    varchar(255) NOT NULL,
+  zipcode   integer
+);
+CREATE TABLE enrollments (
+  id           serial PRIMARY KEY,
+  student_id   integer,
+  class_id    integer,
+  grade varchar(255)
+);
+CREATE TABLE classes (
+  id           serial PRIMARY KEY,
+  name  varchar(255) NOT NULL,
+  credits integer
+);
